fa7a600280521bb26e9977aa9c0f19fb3eba338a
Rohitpradhan <Rohitpradhan0518@gmail.com>
2023-12-07 13:20:55 -0600

    updates

---
+++ scripts/newtodo.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", e => {
+
+    fillUserList();
+
+    const catOption = c => `<option value="${c.id}">${c.name}</option>`;
+
+    fetch("http://localhost:8083/api/categories")
+        .then(r => r.json())
+        .then(cats => cats.forEach(c => catList.innerHTML += catOption(c)));
+
+    //// GOAL IS TO GENERATE AN ENCODED FORM TO SEND AS THE "body"    
+    //userid=1&category=1&priority=Medium&description=Make+Tea&deadline=2023-12-06
+
+    saveButton.addEventListener("click", e => {
+
+        // LOOP WAY OF GATHERING FORM DATA
+        const elements = document.querySelectorAll("main [name]");
+        const content = [...elements].map(e => `${encodeURIComponent(e.name)}=${encodeURIComponent(e.value)}`).join("&");
+
+        // LINEAR WAY OF GATHERING FORM DATA
+        // const content = new FormData();
+        // content.append("userid", userList.value);
+        // content.append("category", catList.value);
+        // content.append("priority", priorityList.value);
+        // content.append("description", description.value);
+        // content.append("deadline", deadline.value);
+        // console.log(content)
+        //// FormData() NOT WORKING WITH ANY OR NONE FOR CONTENT TYPE
+
+        fetch("http://localhost:8083/api/todos/", {
+            method: "POST", body: content,
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded" //"application/json"
+            }
+        }).then(r => {
+            location="http://localhost:3000/todos.html"
+        });
+
+    }); //END SAVE
+});//END LOADED
---
+++ scripts/todos.js
@@ -0,0 +1,34 @@
+function todo(t) {
+    // const completed = t.completed ? "[X]" : "[&nbsp;]";
+    
+    // let completed = "[&nbsp;]"
+    // if(t.completed){
+    //     completed = "[X]"
+    // }
+    const completed = `<img src="./images/check_box_${t.completed}.svg">Completed`
+    return `
+    <div class="todo card">
+        <div class="id">        ${t.id}</div>
+        <div class="userid">     ${t.userid}</div>
+        <div class="category">   ${t.category}</div>
+        <div class="description">${t.description}</div>
+        <div class="deadline">   ${t.deadline}</div>
+        <div class="priority">   ${t.priority}</div>
+        <div class="completed">  ${completed}</div>
+    </div>
+    `;
+}
+
+document.addEventListener("DOMContentLoaded", e => {
+    
+    fillUserList();
+
+    userList.addEventListener("change", e => {
+        fetch("http://localhost:8083/api/todos/")
+            .then(r => r.json())
+            .then(todos => {
+                todoList.innerHTML = todos.filter(t => t.userid == userList.value).map(todo).join("");
+            })
+    });//END USER CHANGE
+
+});//END OF LOADED
